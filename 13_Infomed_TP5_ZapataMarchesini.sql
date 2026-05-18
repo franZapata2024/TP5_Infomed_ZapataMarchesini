1d424d8a422b3c8c97c13fc48cd5ceb0defb0c81
franZapata2024 <frzapata@itba.edu.ar>
2026-05-18 14:00:12 -0300

    Create 13_Infomed_TP5_ZapataMarchesini.sql
    Consigna 13

---
+++ 13_Infomed_TP5_ZapataMarchesini.sql
@@ -0,0 +1,9 @@
+-- PUNTO 13
+-- Medicamentos prescritos más de una vez por el médico ID 2
+
+SELECT m.nombre, COUNT(*) AS cantidad
+FROM Recetas r
+JOIN Medicamentos m ON r.id_medicamento = m.id_medicamento
+WHERE r.id_medico = 2
+GROUP BY m.nombre
+HAVING COUNT(*) > 1;
