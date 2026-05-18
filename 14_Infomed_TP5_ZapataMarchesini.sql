6f4af759d7d95ffdce078200243b0bb9a8459ca5
franZapata2024 <frzapata@itba.edu.ar>
2026-05-18 14:00:52 -0300

    Create 14_Infomed_TP5_ZapataMarchesini.sql
    Consigna 14

---
+++ 14_Infomed_TP5_ZapataMarchesini.sql
@@ -0,0 +1,9 @@
+
+-- PUNTO 14
+-- Cantidad total de recetas recibidas por cada paciente
+
+SELECT p.nombre, COUNT(*) AS total_recetas
+FROM Recetas r
+JOIN Pacientes p ON r.id_paciente = p.id_paciente
+GROUP BY p.nombre
+ORDER BY total_recetas DESC;
