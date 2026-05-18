65474c3781878138c2e35f1066aa527f03c01237
franZapata2024 <frzapata@itba.edu.ar>
2026-05-18 15:15:54 -0300

    Delete 04_Infomed_TP5_ZapataMarchesini.sql

--- 04_Infomed_TP5_ZapataMarchesini.sql
+++
@@ -1,8 +0,0 @@
--- PUNTO 4
--- Actualizar dirección de Luciana Gómez
-
-UPDATE Pacientes
-SET calle = 'Calle Corrientes',
-    numero = '500',
-    ciudad = 'Buenos Aires'
-WHERE nombre = 'Luciana Gómez';
